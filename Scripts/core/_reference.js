37c368067141836877a66c9285d277c245370bf5
anant30 <Anant@Anants-MacBook-Air.local>
2016-01-28 19:59:05 -0500

    initial commit

---
+++ Scripts/core/_reference.js
@@ -0,0 +1,4 @@
+/// <reference path="../../typings/tsd.d.ts" />
+/// <reference path="../objects/person.ts" />
+/// <reference path="../objects/student.ts" />  
+//# sourceMappingURL=_reference.js.map
